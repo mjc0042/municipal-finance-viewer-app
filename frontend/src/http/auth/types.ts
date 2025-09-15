bf07903a0db377a7c0eb154490f5642f9a669b5d
mjc0042 <mjc0042@gmail.com>
2025-09-15 15:08:04 -0400

    Cleaning front auth types, user schema.

--- frontend/src/http/auth/types.ts
+++ frontend/src/http/auth/types.ts
@@ -2,7 +2,7 @@ export interface RegisterData {
   email: string;
   first_name: string;
   last_name: string;
-  organization?: string;
+  organization: string;
   password: string;
   password_confirmation: string;
 }
@@ -11,8 +11,8 @@ export interface User {
   id: number;
   email: string;
   firstName: string;
-  lastName?: string;
-  organization?: string;
-  subscriptionTier?: string;
-  credits?: number;
+  lastName: string;
+  organization: string;
+  subscriptionTier: string;
+  credits: number;
 }
